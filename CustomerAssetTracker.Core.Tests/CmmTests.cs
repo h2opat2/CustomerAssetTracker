0d1b1dabf6a985d5143362bfca010011a2fbf946
Honza <h2opat2@gmail.com>
2025-05-22 12:53:10 +0200

    Add simple tests generated by GitHub Copilot

---
+++ CustomerAssetTracker.Core.Tests/CmmTests.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using CustomerAssetTracker.Core;
+using NUnit.Framework;
+
+namespace CustomerAssetTracker.Core.Tests
+{
+    [TestFixture]
+    public class CmmTests
+    {
+        // Komentář: Testuje, zda konstruktor správně nastaví MachineType.
+        [Test]
+        public void Constructor_ShouldSetMachineTypeToCmm()
+        {
+            // Arrange
+            var cmm = new Cmm();
+
+            // Act (již proběhlo v Arrange)
+
+            // Assert
+            Assert.That(cmm.MachineType, Is.EqualTo(Machine.MachineTypes.Cmm), "MachineType should be Cmm.");
+        }
+
+        // Komentář: Testuje, zda nastavení kladné hodnoty pro SizeX funguje.
+        [Test]
+        public void SizeX_ShouldSetPositiveValue()
+        {
+            // Arrange
+            var cmm = new Cmm();
+            int expectedSize = 100;
+
+            // Act
+            cmm.SizeX = expectedSize;
+
+            // Assert
+            Assert.That(cmm.SizeX, Is.EqualTo(expectedSize), "SizeX should be set to the provided positive value.");
+        }
+
+        // Komentář: Testuje, zda nastavení záporné hodnoty pro SizeX vyvolá výjimku.
+        // Atribut [TestCase] umožňuje spouštět test s různými vstupními daty.
+        // Atribut [Throws] ověřuje, že metoda vyvolá specifickou výjimku.
+        [TestCase(-1)]
+        [TestCase(-100)]
+        public void SizeX_ShouldThrowArgumentOutOfRangeException_WhenNegativeValue(int negativeSize)
+        {
+            // Arrange
+            var cmm = new Cmm();
+
+            // Act & Assert: Používáme Assert.Throws pro ověření výjimky.
+            // Lambda výraz () => cmm.SizeX = negativeSize; je kód, který očekáváme, že vyvolá výjimku.
+            Assert.Throws<ArgumentOutOfRangeException>(() => cmm.SizeX = negativeSize,
+                "Setting SizeX to a negative value should throw an ArgumentOutOfRangeException.");
+        }
+
+        // Komentář: Podobné testy bys napsal/a i pro SizeY a SizeZ.
+        // [Test]
+        // public void SizeY_ShouldSetPositiveValue() { ... }
+        // [TestCase(-1)]
+        // public void SizeY_ShouldThrowArgumentOutOfRangeException_WhenNegativeValue(int negativeSize) { ... }
+        // atd.
+
+        // Komentář: Testovací metoda pro ověření GetFullName() na základní třídě Machine.
+        // Můžeme ji testovat zde, protože Cmm dědí z Machine.
+        [Test]
+        public void GetFullName_ShouldReturnCorrectFormat()
+        {
+            // Arrange
+            var cmm = new Cmm();
+            cmm.Name = "CMM_XYZ";
+            cmm.SerialNumber = "SN12345";
+            cmm.Manufacturer = "LK";
+            // MachineType je již nastaven konstruktorem Cmm() na MachineTypes.Cmm
+
+            string expectedFullName = "Cmm: CMM_XYZ, SN: SN12345 (LK)";
+
+            // Act
+            string actualFullName = cmm.GetFullName();
+
+            // Assert
+            Assert.That(actualFullName, Is.EqualTo(expectedFullName), "GetFullName should return the correct formatted string.");
+        }
+    }
+}
---
+++ CustomerAssetTracker.Core.Tests/MachineTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using CustomerAssetTracker.Core;
+using NUnit.Framework;
+
+namespace CustomerAssetTracker.Core.Tests
+{
+    [TestFixture]
+    public class MachineTests
+    {
+        [Test]
+        public void Constructor_InitializesCollections()
+        {
+            // Act
+            var machine = new Machine();
+
+            // Assert
+            Assert.That(machine.Licenses, Is.Not.Null);
+            Assert.That(machine.ServiceRecords, Is.Not.Null);
+            Assert.That(machine.Licenses, Is.Empty);
+            Assert.That(machine.ServiceRecords, Is.Empty);
+        }
+
+        [Test]
+        public void GetFullName_ReturnsExpectedFormat()
+        {
+            // Arrange
+            var machine = new Machine
+            {
+                MachineType = Machine.MachineTypes.Cmm,
+                Name = "TestName",
+                SerialNumber = "SN123",
+                Manufacturer = "TestManufacturer"
+            };
+
+            // Act
+            var fullName = machine.GetFullName();
+
+            // Assert
+            Assert.That(fullName, Is.EqualTo("Cmm: TestName, SN: SN123 (TestManufacturer)"));
+        }
+
+        [Test]
+        public void GetFullName_HandlesNullProperties()
+        {
+            // Arrange
+            var machine = new Machine();
+
+            // Act
+            var fullName = machine.GetFullName();
+
+            // Assert
+            Assert.That(fullName, Is.EqualTo("Cmm: , SN:  ()"));
+        }
+    }
+}
